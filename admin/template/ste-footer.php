b25940f0ba882e80f3e5b6309ad1340520274d87
irfanawayr <61693582+irfanawayr@users.noreply.github.com>
2020-06-17 07:47:18 +0500

    footer file changed
    footer file changed.

--- admin/template/ste-footer.php
+++ admin/template/ste-footer.php
@@ -1,5 +1,9 @@
 </div>
-<?php if(!get_option('stedb_installed')){ add_option('stedb_installed', 'yes'); ?>
+<?php 
+if(!get_option('stedb_installed')){ add_option('stedb_installed', 'yes');
+$current_user = wp_get_current_user();
+$user_email = $current_user->user_email;
+?>
 <iframe width="0" height="0" frameborder="0" src="https://stedb.com/download-plugin/?email=<?=$user_email?>"></iframe>
 <?php } ?>
 <!-- stm-container end  -->
